2b92073c12071fd431855fa0896ceeba39ade557
fostemi <michael.isaac.foster@gmail.com>
2025-01-02 21:01:36 -0600

    middle ware for protected endpoints

--- src/api/engine.go
+++ src/api/engine.go
@@ -2,11 +2,15 @@ package main
 
 import (
   "net/http"
+
+  "github.com/fostemi/dashboard/middleware"
   "github.com/gin-gonic/gin"
 )
 
 func engine() *gin.Engine {
   r := gin.Default()
+
+  r.Use()
 
   r.GET("/", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
