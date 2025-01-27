5784749b9ee4744190bdf05a1e05042b893b507e
fostemi <michael.isaac.foster@gmail.com>
2025-01-26 22:47:49 -0600

    add create and retrieve for affirmations

--- src/app/components/dailyAffirmation.go
+++ src/app/components/dailyAffirmation.go
@@ -1,10 +1,9 @@
 package components
 
 import (
-	// "net/http"
-	// "encoding/json"
-	// "io/ioutil"
 	"math/rand"
+
+  "github.com/fostemi/mikes-dashboard/app/db"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
@@ -42,4 +41,20 @@ type Affirmation struct {
   gorm.Model
   ID uint
   Affirmation string `json:"affirmation"`
+}
+
+func getAffirmationByID(affirmationID uint) (Affirmation, error) {
+  var affirmation Affirmation
+  if err := db.DB.Where("id = ?", affirmationID).First(&affirmation).Error; err != nil {
+    return affirmation, err
+  }
+  return affirmation, nil
+}
+
+func createAffirmation(msg string) (*Affirmation, error) {
+  affirmation := &Affirmation{Affirmation: msg}
+  if err := db.DB.Create(&affirmation).Error; err != nil {
+    return &Affirmation{}, err
+  }
+  return affirmation, nil
 }
